3096b68435bcf203b25f5ec6e4bd6868b972010a
Tempta36 <tempta36@163.com>
2017-06-17 21:04:19 +0800

    finish all

--- palaceMove.js
+++ palaceMove.js
@@ -3,83 +3,27 @@
  */
 window.onload = function () {
     var palace_unit = document.getElementsByClassName('palace_unit');
-    var palace_move = document.getElementsByClassName('palace_move');
+    var palace_row = document.getElementsByClassName('palace_row');
+    var palace_col = document.getElementsByClassName('palace_col');
     var palace_reset=document.getElementsByClassName('palace_reset')[0];
     var palace_back=document.getElementsByClassName('palace_back')[0];
     var palace_tr=document.getElementsByClassName('palace_tr');
     var unit_length=palace_unit.length;
-    var move_length=palace_move.length;
+    var row_length=palace_row.length;
+    var col_length=palace_col.length;
 
     var px=0;
     var py=0;
 
 
-    //鼠标在第一行落下时，行/列互换
-    for(var j=0;j<move_length-3;j++){
-        palace_move[j].index=j;
-        palace_move[j].onmousedown=function(ev){
-            var oEvent=ev||event;
-            var marginLeft=offSet(palace_move[0]).left;
-            var marginTop=offSet(palace_move[0]).top
-            var downLeft=oEvent.clientX-marginLeft;
-            var downTop=oEvent.clientY-marginTop;
-
-            var downDom=gainDom(downLeft,downTop);
-
-            var tmp=new CreateDiv();
-            tmp.createDiv(palace_move[this.index]);
-
-            document.onmousemove=function(ev){
-                var oEvent=ev||event;
-
-                var l=oEvent.clientX-px;
-                var t=oEvent.clientY-py;
-
-                tmp.tmp.style.left=l-6+'px';
-                tmp.tmp.style.top=t-6+'px';
-            };
-
-            document.onmouseup=function(ev){
-                var oEvent=ev||event;
-
-                var upLeft=oEvent.clientX-marginLeft;
-                var upTop=oEvent.clientY-marginTop;
-
-                var upDom=gainDom(upLeft,upTop);
-                //鼠标在第一列抬起，进行行列互换
-                if(upDom.col==0){
-                    var row=upDom.row;
-                    var col=downDom.col;
-                    dataExchange(row,col);
-
-                    palace_back.onclick=function(){
-                        dataExchange(row,col);
-                    };
-                    palace_reset.onclick=function(){
-                        window.location.reload();
-                    };
-                }
-
-                document.onmousemove=null;
-                document.onmouseup=null;
-                tmp.removeDiv();
-            };
-            return false;
-        };
-    }
-
-
-
-
-    //单元格互换
     for(var i=0;i<unit_length;i++){
         palace_unit[i].index=i;
-        //鼠标在第二三行落下
+
         palace_unit[i].onmousedown=function(ev){
             var oEvent=ev||event;
 
-            var marginLeft=offSet(palace_move[0]).left;
-            var marginTop=offSet(palace_move[0]).top;
+            var marginLeft=offSet(palace_row[0]).left;
+            var marginTop=offSet(palace_row[0]).top;
             var downLeft=oEvent.clientX;
             var downTop=oEvent.clientY;
 
@@ -110,35 +54,49 @@ window.onload = function () {
                 //交换两个宫格的内容
 
                 if(gainDom(offsetLeft,offsetTop)!=0){
-                    //鼠标在除第一行之外的单元格抬起，进行单元格互换
-                    if(gainDom(offsetLeft,offsetTop).row!=0){
-                        var upDom=gainDom(offsetLeft,offsetTop).obj;
-                        var downDom=gainDom(downLeft-marginLeft,downTop-marginTop).obj;
-                        var tmpHtml=upDom.innerHTML;
-                        upDom.innerHTML=downDom.innerHTML;
-                        downDom.innerHTML=tmpHtml;
+                    var upDom=gainDom(offsetLeft,offsetTop);
+                    var downDom=gainDom(downLeft-marginLeft,downTop-marginTop);
+
+                    //鼠标在第一列落下第一列抬起，进行 行行互换
+                    if(downDom.col==0&&upDom.col==0){
+                        var up_row=upDom.row;
+                        var down_row=downDom.row;
+                        dataExchange(up_row,down_row,1);
+
+                        palace_back.onclick=function(){
+                            dataExchange(up_row,down_row,1);
+                        };
+                        palace_reset.onclick=function(){
+                            window.location.reload();
+                        };
+                    }else if(downDom.row==0&&upDom.row==0){
+                        //鼠标在第一行落下第一行抬起，进行 列列互换
+                        var up_col=upDom.col;
+                        var down_col=downDom.col;
+                        dataExchange(up_col,down_col,0);
+
+                        palace_back.onclick=function(){
+                            dataExchange(up_col,down_col,0);
+                        };
+                        palace_reset.onclick=function(){
+                            window.location.reload();
+                        };
+                    }else if((downDom.col==0&&upDom.col!=0)||(downDom.row==0&&upDom.row!=0) || //进行单元格互换
+                        downDom.row!=0|| downDom.col!=0){
+                        var tmpHtml=upDom.obj.innerHTML;
+                        upDom.obj.innerHTML=downDom.obj.innerHTML;
+                        downDom.obj.innerHTML=tmpHtml;
 
                         palace_reset.onclick=function(){
                             window.location.reload();
                         };
                         palace_back.onclick=function(){
-                            tmpHtml=upDom.innerHTML;
-                            upDom.innerHTML=downDom.innerHTML;
-                            downDom.innerHTML=tmpHtml;
-                        };
-                    }else {
-                    //鼠标在第一行的单元格抬起，进行行列互换
-                        var row=gainDom(downLeft-marginLeft,downTop-marginTop).row;
-                        var col=gainDom(offsetLeft,offsetTop).col;
-                        dataExchange(row,col);
-
-                        palace_back.onclick=function(){
-                            dataExchange(row,col);
-                        };
-                        palace_reset.onclick=function(){
-                            window.location.reload();
+                            tmpHtml=upDom.obj.innerHTML;
+                            upDom.obj.innerHTML=downDom.obj.innerHTML;
+                            downDom.obj.innerHTML=tmpHtml;
                         };
                     }
+
                 }
 
 
@@ -227,47 +185,51 @@ window.onload = function () {
         };
     }
 
-    function dataExchange(row,col){
-        //row表示获取第row行
-        //col表示获取第col列
+    function dataExchange(up,down,way){
         var isGain=1;   //1表示获取值，0表示赋值
 
-        var length=0;
-        var rowInnerHtmlList=[];
-        var colInnerHtmlList=[];
+        var length=4;
+        var upInnerHtmlList=[];
+        var downInnerHtmlList=[];
 
         traverse();
-
-        for(var n=0;n<length;n++){
-            var tmp=rowInnerHtmlList[n];
-            rowInnerHtmlList[n]=colInnerHtmlList[n];
-            colInnerHtmlList[n]=tmp;
-        }
-        isGain=0;
-
         traverse();
 
         function traverse(){
-            var arow=document.getElementsByClassName('palace_tr')[row];
-            var cols=arow.getElementsByClassName('palace_td');
-            length=cols.length;
-            for(var k=0;k<length;k++){
+            if(!way){
+                var rows=document.getElementsByClassName('palace_tr');
+
                 if(isGain){
-                    rowInnerHtmlList[k]=cols[k].innerHTML;
+                    isGain=0;
+                    for(var k=0;k<length;k++) {
+                        downInnerHtmlList[k] = rows[k].getElementsByClassName('palace_unit')[down].innerHTML;
+                        upInnerHtmlList[k] = rows[k].getElementsByClassName('palace_unit')[up].innerHTML;
+                    }
                 }else{
-                    cols[k].innerHTML=rowInnerHtmlList[k];
+                    for(var k=0;k<length;k++) {
+                        rows[k].getElementsByClassName('palace_unit')[down].innerHTML = upInnerHtmlList[k];
+                        rows[k].getElementsByClassName('palace_unit')[up].innerHTML = downInnerHtmlList[k];
+                    }
                 }
+            }else{
+                var upRow=document.getElementsByClassName('palace_tr')[up];
+                var downRow=document.getElementsByClassName('palace_tr')[down];
+
+                if(isGain){
+                    isGain=0;
+                    for(var m=0;m<length;m++) {
+                        upInnerHtmlList[m] = upRow.getElementsByClassName('palace_unit')[m].innerHTML;
+                        downInnerHtmlList[m] = downRow.getElementsByClassName('palace_unit')[m].innerHTML;
+                     }
+                }else{
+                    for(var m=0;m<length;m++) {
+                        upRow.getElementsByClassName('palace_unit')[m].innerHTML = downInnerHtmlList[m];
+                        downRow.getElementsByClassName('palace_unit')[m].innerHTML = upInnerHtmlList[m];
+                    }
+                }
+
             }
 
-            var rows=document.getElementsByClassName('palace_tr');
-            for(var m=0;m<length;m++){
-                var acol=rows[m].getElementsByClassName('palace_td')[col];
-                if(isGain){
-                    colInnerHtmlList[m]=acol.innerHTML;
-                }else{
-                    acol.innerHTML=colInnerHtmlList[m];
-                }
-            }
         }
     }
 
